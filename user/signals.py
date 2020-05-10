55027047621a0b662986ae04aa40f18fc7579ddc
diptarag1 <diptarag1@gmail.com>
2020-05-11 01:11:36 +0530

    Adding all the basic functionalities

--- user/signals.py
+++ user/signals.py
@@ -3,11 +3,14 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
 
-@receiver(post_save,sender=User)
-def create_profile(sender,instance,created,**kwargs):
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        instance.profile.save()
 
-@receiver(post_save,sender=User)
-def save_profile(sender,instance,**kwargs):
-    instance.userprofile.save()
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
--- user/views.py
+++ user/views.py
@@ -1,50 +1,60 @@
-from django.urls import reverse
-from django.http import HttpResponse, JsonResponse
-try:
-    from django.utils import simplejson as json
-except ImportError:
-    import json
-from django.template.loader import render_to_string
-from django.utils import timezone
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, ProfileCreateForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import DetailView, ListView
 from .models import Profile
-from django.shortcuts import render, get_object_or_404, redirect
-from .forms import ProfileForm,UserRegisterForm
-from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import (
-    ListView, DetailView, DeleteView, CreateView, UpdateView)
-from django.contrib.auth.models import User
-from django.views.generic.edit import FormMixin
+from Post.models import Post
+from Post.views import PostListView
 
 # Create your views here.
-def index(request):
-    return render(request,'index.html')
-
-
 def register(request):
-	if request.method =='POST':
-		form = UserRegisterForm(request.POST)
-		if form.is_valid():
-			form.save(commit=True)
-			username = form.cleaned_data.get('username')
-			# messages.success(request, f'{ username }')
-			return redirect('/login')
+	if(request.method == 'POST'):
+		form1 = UserRegisterForm(request.POST)
+		if(form1.is_valid()):
+			form1.save()
+			username = form1.cleaned_data.get('username')
+			messages.success(request, f'Account created for {username}! You can now log in')
+			return redirect('login')
 	else:
-		form = UserRegisterForm()
-	return render(request, 'user/registration/register.html', {'form': form})
+		form1 = UserRegisterForm()
+		form2 = ProfileCreateForm()
+	context = {
+		'form1': form1,
+	}
+	return render(request, 'users/register.html', context)
 
-def userdetail(request, slug):
-    userd = User.objects.filter(username__iexact=slug)
-    if request.method == "POST":
-        form = ProfileForm(request.POST, instance=request.user.userprofile)
-        if form.is_valid():
-            form.save()
-            return redirect('profile', slug=slug)
+@login_required
+def profile(request):
+	if(request.method == 'POST'):
+		#u_form = UserUpdateForm(request.POST, instance = request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance = request.user.profile)
 
-    else:
-        form = ProfileForm(instance=request.user.userprofile)
-    context = {
-        'users': userd[0],
-        'pform': form
-    }
-    return render(request, "user/profile.html", context)
+		if(p_form.is_valid()):
+			#u_form.save()
+			p_form.save()
+			messages.success(request, 'Account has been updated.')
+			return redirect('profile')
+	else:
+		p_form = ProfileUpdateForm(instance = request.user.profile)
+
+	context = {
+		'pform':p_form
+	}
+	context['posts'] = Post.objects.filter(author__profile = request.user.profile)
+	return render(request, 'users/profile.html', context)
+
+
+
+
+class ProfileDetailView(DetailView):
+	model = Profile
+	template_name = 'users/profile_other.html'
+
+	def get_context_data(self, **kwargs):
+		# Call the base implementation first to get a context
+		context = super().get_context_data(**kwargs)
+		# Add in a QuerySet of all the books
+		context['posts'] = Post.objects.filter(author__profile = self.object)
+		return context
